628f0765d61edf4fbc832f1c1f1525d92dc5eab8
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2020-09-23 11:37:56 +0700

    Video #12

---
+++ constant.php
@@ -0,0 +1,15 @@
+<?php
+
+// define('NAMA', 'Ridha Ahmad Firdaus');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 19;
+// echo UMUR;
+
+class Coba {
+    const NAMA = 'Ridha Ahmad Firdaus';
+}
+
+echo Coba::NAMA;
